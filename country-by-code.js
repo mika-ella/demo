7d0198c2cadc2fa49ed76d08e2c32e95f8bbc097
Jayson <aguilarjayson19300@gmail.com>
2023-02-21 15:18:00 -0800

    Add list of ountries by country code

--- country-by-code.js
+++ country-by-code.js
@@ -1,9 +1,11 @@
 const regionNames = new Intl.DisplayNames(
-    ['GB'], {type: 'region'}
+    ['en'], {type: 'region'}
   );
   
   console.log(regionNames.of('US')); // 👉️ "United States"
   console.log(regionNames.of('GB')); // 👉️ "United kingdom"
   console.log(regionNames.of('DE')); // 👉️ "Germany"
   console.log(regionNames.of('AU')); // 👉️ "Australia"
+  console.log(regionNames.of('PH')); // 👉️ "Philippines"
+  console.log(regionNames.of('BD')); // 👉️ "Bangladesh"
   
