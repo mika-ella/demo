3e5fe8d3ce3460a63a8a971958134c7027b0bce9
Jean Carla <jean.carla.guarra@gmail.com>
2023-02-22 15:07:06 +0800

    revert

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,5 @@
-const countryListUpperCase = ['PHILIPPINES', 'CHINA', 'AUSTRALIA', 'RUSSIA', 'JAPAN' ];
-countryListUpperCase.forEach(function (element, index, array){
-    array[index] = element.toLowerCase();
-
-});
-console.log(countryListUpperCase);
+const countryList = ['philippines', 'china', 'australia', 'russia', 'japan' ];
+const countryListUpperCase = ['PHILIPPINES', 'CHINA', 'AUSTRALIA', 'RUSSIA', 'JAPAN' ]
 
 //map (pat) - manipulate the items in the countryList so that the items will become uppercase using map method. Result (array).
 
@@ -13,4 +9,4 @@ console.log(countryListUpperCase);
 
 //find (marvin) - find country in countryList that has the most number of characters using find method. Result (item/undefined).
 
-//some (jayson) - check if there is an item in the countryList that has 'p' character in it using some method. Result (true/false). 
+//some (jayson) - check if there is an item in the countryList that has 'p' character in it using some method. Result (true/false). ß
