a8c57e99496508fda44fa297afb80b30fee06ba2
mikaswr <mikaellajarapa@gmail.com>
2023-02-22 11:53:42 +0800

    add array methods activity

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,12 @@
-console.log('Javascript')
+const countryList = ['philippines', 'china', 'australia', 'russia', 'japan' ];
+const countryListUpperCase = ['PHILIPPINES', 'CHINA', 'AUSTRALIA', 'RUSSIA', 'JAPAN' ];
+
+//map (pat) - manipulate the items in the countryList so that the items will become uppercase using map method. Result (array).
+
+//forEach (jean) - manipulate the items in the countryListUpperCase so that the items will become lowercase using forEach method. Result (display result of every iteration).
+
+//filter (red) - filter out country in countryList that includes 'i' and 'a' characters and with more than 5 characters using filter method. Result (array).
+
+//find (marvin) - find country in countryList that has the most number of characters using find method. Result (item/undefined).
+
+//some (jayson) - check if there is an item in the countryList that has 'p' character in it using some method. Result (true/false). 
