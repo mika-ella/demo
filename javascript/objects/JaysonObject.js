790b81e988ffb6e80fdca3d9a9139e0c366b4c91
AguilarJayson <aguilarjayson19300@gmail.com>
2023-02-23 15:14:59 +0800

    Add JS Array Object

---
+++ javascript/objects/JaysonObject.js
@@ -0,0 +1,29 @@
+const products = [
+    { name: "Product A", price: 20, category: "Electronics" },
+    { name: "Product E", price: 60, category: "Clothes" },
+    { name: "Product G", price: 80, category: "Clothes" },
+    { name: "Product H", price: 90, category: "Electronics" },
+    { name: "Product C", price: 40, category: "Electronics" },
+    { name: "Product D", price: 50, category: "Clothes" },
+    { name: "Product B", price: 30, category: "Clothes" },
+    { name: "Product F", price: 70, category: "Electronics" },
+  ];
+  
+  // Expected array output - JAYSON
+// [
+//    { name: "Product A", price: 30, category: "Electronics" },
+//    { name: "Product E", price: 70, category: "Clothes" },
+//    { name: "Product G", price: 90, category: "Clothes" },
+//    { name: "Product H", price: 100, category: "Electronics" },
+//    { name: "Product C", price: 50, category: "Electronics" },
+//    { name: "Product D", price: 60, category: "Clothes" },
+//    { name: "Product B", price: 40, category: "Clothes" },
+//    { name: "Product F", price: 80, category: "Electronics" },
+// ]
+
+const plusten = products.map((product) => {
+    product.price += 10;
+    return product;
+  });
+
+console.log(plusten);
