da5b4ab64632bc540f0e4253b230e942b6818151
mikaswr <mikaellajarapa@gmail.com>
2023-02-23 09:10:49 +0800

    create new array methods activity

---
+++ javascript/methods.js
@@ -0,0 +1,8 @@
+
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant', 12, 15, 99, 87, 1];
+
+//1. Expected output: Array [12, 15, 99, 87, 1]
+
+//2. Expected output: Array ['ant', 'bison', 'camel', 'duck', 'elephant', '12', '15', '99', '87', '1']
+
+// choose the appropriate array method to solve the problems.
