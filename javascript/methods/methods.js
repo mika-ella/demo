c93dc2fb43e7833acb9ff652095235906663ab99
Jean Carla <jean.carla.guarra@gmail.com>
2023-02-23 16:00:45 +0800

    add array methods

--- javascript/methods/methods.js
+++ javascript/methods/methods.js
@@ -7,4 +7,4 @@ const animals = ['ant', 'bison', 'camel', 'duck', 'elephant', 12, 15, 99, 87, 1]
 
 //3. Expected output: Array ['bison', 'camel', 'elephant']
 
-// choose the appropriate array method to solve the problems.
+// choose the appropriate array method to solve the problem. 
