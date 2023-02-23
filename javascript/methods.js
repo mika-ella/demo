3305d992254b4c935ecfa7463639bc93c3f238e3
mikaswr <mikaellajarapa@gmail.com>
2023-02-23 09:34:02 +0800

    add another activity

--- javascript/methods.js
+++ javascript/methods.js
@@ -5,4 +5,6 @@ const animals = ['ant', 'bison', 'camel', 'duck', 'elephant', 12, 15, 99, 87, 1]
 
 //2. Expected output: Array ['ant', 'bison', 'camel', 'duck', 'elephant', '12', '15', '99', '87', '1']
 
+//3. Expected output: Array ['bison', 'camel', 'elephant']
+
 // choose the appropriate array method to solve the problems.
