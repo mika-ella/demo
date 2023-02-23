14ce0c1c175cb5866807cbe2afc6b6ea022c871f
mikaswr <mikaellajarapa@gmail.com>
2023-02-23 14:21:28 +0800

    create algorithm activity

---
+++ javascript/algorithms.js
@@ -0,0 +1,5 @@
+// Min, Max, Length and Average of an array of numbers - JAYSON
+// Eliminating the Odds in an array of integers and store even numbers in other array - RED
+// Adding all the floating numbers in an array - PAT
+// Identifying if a number is prime in an array, return prime numbers - JEAN
+// Convert all floating numbers to an integer from an Array - MARVIN
