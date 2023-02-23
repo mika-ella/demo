4eee4d814c3744b20a39072a3d09180fec82aa9e
rekesan <redkeans@gmail.com>
2023-02-23 11:15:10 +0800

    filter and map array methods used to satisfy expected outputs

--- javascript/methods.js
+++ javascript/methods.js
@@ -2,10 +2,14 @@
 const animals = ['ant', 'bison', 'camel', 'duck', 'elephant', 12, 15, 99, 87, 1];
 
 //1. Expected output: Array [12, 15, 99, 87, 1]
-console.log(animals.filter(item => Number.isInteger(item)));
+console.log(animals.filter(item => typeof item == 'number'));
 
 //2. Expected output: Array ['ant', 'bison', 'camel', 'duck', 'elephant', '12', '15', '99', '87', '1']
-console.log(animals)
+console.log(animals.map(item => {
+    if (typeof item == 'number')
+        return item.toString();
+    return item;
+}))
 
 //3. Expected output: Array ['bison', 'camel', 'elephant']
 console.log(animals.filter(item => typeof item == 'string' && item.length > 4));
