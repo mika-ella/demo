d4eae538fe6880b5678ecf648506ae58f103c858
Patricia Lofamia <101615967+ImPatriciaLofamia@users.noreply.github.com>
2023-02-23 09:34:47 +0800

    refactor array map method to uppercase

--- javascript/map-array.js
+++ javascript/map-array.js
@@ -1,8 +1,5 @@
 const countryList = ['philippines', 'china', 'australia', 'russia', 'japan' ];
 const countryListUpperCase = ['PHILIPPINES', 'CHINA', 'AUSTRALIA', 'RUSSIA', 'JAPAN' ];
 
-function Result(array) {
-    const uppercasedList = array.map(array => array.toUpperCase()); 
-    return uppercasedList;
-}
-console.log(Result(countryList));
+const uppercasedList = countryList.map(country => country.toUpperCase()); 
+console.log(uppercasedList);
