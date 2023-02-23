e114bb77973e4bd983477f9c42a4c39c704c18df
rekesan <redkeans@gmail.com>
2023-02-23 09:54:53 +0800

    used filter method to satisfy expected output

--- javascript/methods.js
+++ javascript/methods.js
@@ -2,9 +2,12 @@
 const animals = ['ant', 'bison', 'camel', 'duck', 'elephant', 12, 15, 99, 87, 1];
 
 //1. Expected output: Array [12, 15, 99, 87, 1]
+console.log(animals.filter(item => Number.isInteger(item)));
 
 //2. Expected output: Array ['ant', 'bison', 'camel', 'duck', 'elephant', '12', '15', '99', '87', '1']
+console.log(animals)
 
 //3. Expected output: Array ['bison', 'camel', 'elephant']
+console.log(animals.filter(item => typeof item == 'string' && item.length > 4));
 
 // choose the appropriate array method to solve the problems.
