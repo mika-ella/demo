6e6c635854b59d82bfa8aea05d1b1627a3db7409
AguilarJayson <aguilarjayson19300@gmail.com>
2023-02-23 14:13:58 +0800

    Add JS Method/Fix second line of Code based from the required output.

--- javascript/jaysonmethod.js
+++ javascript/jaysonmethod.js
@@ -1,6 +1,6 @@
 const animals = ['ant', 'bison', 'camel', 'duck', 'elephant', 12, 15, 99, 87, 1];
 
-const num = animals.filter((value) => typeof value !== 'number');
+const num = animals.filter((value) => typeof value !== 'string');
 const ftr = animals.filter((value) => typeof value !== 'number' && String(value).includes('e') || String(value).includes('i'));
 
 console.log(num);
