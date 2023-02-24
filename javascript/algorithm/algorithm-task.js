2781788e45dc6cb5bd6b81bf70649b207f0fb3ae
Jean Carla <jean.carla.guarra@gmail.com>
2023-02-24 10:13:57 +0800

    add algorithm

--- javascript/algorithm/algorithm-task.js
+++ javascript/algorithm/algorithm-task.js
@@ -1,5 +1,16 @@
 // Identifying if a number is prime in an array, return prime numbers - JEAN
-
-const arrNumber = [1,2,3,4,5,11,13,15,20]; 
-const IsPrime = arrNumber.map((prm) => prm.checkPrime)
-console.log(IsPrime);
+/*  1. Create an array that consist of numbers.
+    2. Create a function that will take a single parameter. Inside the function create a for loop
+        that starts in 2 up to the square root of the number 
+    3. Check if the number is divisible by the loop, if it is divisible it will return because the number is not prime.
+    4. If the loop has not found divisible, return true because it is a prime number.
+    5. Create another function, inside the function create an empty array. 
+    6. Used for loop to iterate each number.
+    7. Call the first function. If the number is prime it will add to the empty array.
+    8. Return the array.
+    9. Print the result.
+    
+    
+    
+    
+    */
