95e7c781b261faeb41fad0a6d1a35d4143b3f9e5
AguilarJayson <aguilarjayson19300@gmail.com>
2023-02-24 12:54:04 +0800

    Fix/Issue in naming convention in variable and create the expected output on number two problem

--- javascript/methods/JaysonMethods.js
+++ javascript/methods/JaysonMethods.js
@@ -1,8 +1,16 @@
 const animals = ['ant', 'bison', 'camel', 'duck', 'elephant', 12, 15, 99, 87, 1];
 
 const num = animals.filter((value) => typeof value !== 'string');
-const ftr = animals.filter((value) => typeof value !== 'number' && String(value).includes('e') || String(value).includes('i'));
+
+const convertedAnimals = animals.map(animal => {
+  if (typeof animal === 'number') {
+    return String(animal);
+  }
+  return animal;
+});
+
+const filteredAnimals = animals.filter(animal => typeof animal === 'string' && animal.length >= 3);
 
 console.log(num);
-console.log(animals);
-console.log(ftr);
+console.log(convertedAnimals);
+console.log(filteredAnimals);
