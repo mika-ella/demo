6ec54fbf2010daaf2f2e32b513a3a65ccf806a51
Jean Carla <jean.carla.guarra@gmail.com>
2023-02-24 10:55:20 +0800

    add algorithm that identifying prime numbers

--- javascript/algorithm/algorithm-task.js
+++ javascript/algorithm/algorithm-task.js
@@ -1,16 +1,14 @@
 // Identifying if a number is prime in an array, return prime numbers - JEAN
 /*  1. Create an array that consist of numbers.
-    2. Create a function that will take a single parameter. Inside the function create a for loop
-        that starts in 2 up to the square root of the number 
-    3. Check if the number is divisible by the loop, if it is divisible it will return because the number is not prime.
-    4. If the loop has not found divisible, return true because it is a prime number.
-    5. Create another function, inside the function create an empty array. 
-    6. Used for loop to iterate each number.
-    7. Call the first function. If the number is prime it will add to the empty array.
-    8. Return the array.
-    9. Print the result.
-    
-    
-    
+    2. Create a function that takes a single parameter.
+    3. Use loop to check if the number is divisible in any number from to current number.
+        If it is, return false because it is not a prime number.
+        If it is not, return true because it is a prime number.
+    4. Create another function that takes an array of numbers, within function create an empty array. 
+    5. Used loop to iterate each number.
+    6. Call the first function.
+    7.If the number is prime it will add to the empty array.
+    8. Print the result.
+
     
     */
