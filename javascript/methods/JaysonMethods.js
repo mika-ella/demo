dca23c5c84e4eb85b476e905bc1f54271ecab04b
AguilarJayson <aguilarjayson19300@gmail.com>
2023-02-24 13:04:29 +0800

    Fix naming convention on variable and return the expected output for problem number two.

--- javascript/methods/JaysonMethods.js
+++ javascript/methods/JaysonMethods.js
@@ -1,6 +1,6 @@
 const animals = ['ant', 'bison', 'camel', 'duck', 'elephant', 12, 15, 99, 87, 1];
 
-const num = animals.filter((value) => typeof value !== 'string');
+const number = animals.filter((value) => typeof value !== 'string');
 
 const convertedAnimals = animals.map(animal => {
   if (typeof animal === 'number') {
@@ -11,6 +11,6 @@ const convertedAnimals = animals.map(animal => {
 
 const filteredAnimals = animals.filter(animal => typeof animal === 'string' && animal.length >= 3);
 
-console.log(num);
+console.log(number);
 console.log(convertedAnimals);
 console.log(filteredAnimals);
