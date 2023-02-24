99e63c73f674bef16b41047fd3d2356a7e85d1a4
Jean Carla <jean.carla.guarra@gmail.com>
2023-02-24 09:06:59 +0800

    made an algorithm on identifying prime numbers.

---
+++ javascript/algorithm/algorithm-task.js
@@ -0,0 +1,5 @@
+// Identifying if a number is prime in an array, return prime numbers - JEAN
+
+const arrNumber = [1,2,3,4,5,11,13,15,20]; 
+const IsPrime = arrNumber.map((prm) => prm.checkPrime)
+console.log(IsPrime);
