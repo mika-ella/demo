bf310b96cf50d2d316f994f7cc943c5e0dddc605
Jean Carla <jean.carla.guarra@gmail.com>
2023-02-28 11:26:13 +0800

    add feature using DRY principles

---
+++ javascript/Cprinciples-jean.js
@@ -0,0 +1,16 @@
+function checkGreaterNum(x, y) {
+    if (x > y) {
+      console.log(`${x} is greater than ${y}`);
+    } else {
+      console.log(`${y} is greater than ${x}`);
+    }
+  }
+  
+  let x = 5;
+  let y = 10;
+  checkGreaterNum(x, y);
+  
+  x = 7;
+  y = 3;
+  checkGreaterNum(x, y);
+  
