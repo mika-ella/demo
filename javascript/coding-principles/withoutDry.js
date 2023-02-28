489bb70f06d9229327cce24d26f75f976708a36e
AguilarJayson <aguilarjayson19300@gmail.com>
2023-02-28 11:49:56 +0800

    Build/Function with DRY Principles

--- javascript/coding-principles/withoutDry.js
+++ javascript/coding-principles/withoutDry.js
@@ -1,3 +1,10 @@
-function isEvenOrOdd(num: number): boolean {
-    return num % 2 === 0 ? true : false;
+let numbers = [1.126, 277, 322, 42, 52];
+console.log(numbers[0]);
+console.log(numbers[1]);
+console.log(numbers[2]);
+console.log(numbers[3]);
+console.log(numbers[4]);
+
+function isEvenOrOdd(number){
+    return number % 2 === 0 ? true : false;
   }
