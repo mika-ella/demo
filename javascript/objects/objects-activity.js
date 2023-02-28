5796b1659745498070923e87a708732d762ff3d3
Marvin <marvinvillalon23@gmail.com>
2023-02-28 11:30:09 +0800

    remove unused logs

--- javascript/objects/objects-activity.js
+++ javascript/objects/objects-activity.js
@@ -18,45 +18,10 @@ const products = [
     { name: "Edwin", salary: 70000, department: "IT" },
     { name: "Carlo", salary: 60000, department: "HR" },
     { name: "Edmond", salary: 80000, department: "HR" },
-    //{ name: "Pres", salary: 10000000, department: "Planning" },
+    
   ];
   
-  // Expected array output - MARVIN
-  // [
-  //   { name: "Product A", price: 20, category: "Electronics" }, 
-  //   { name: "Product C", price: 40, category: "Electronics" }, 
-  //   { name: "Product F", price: 70, category: "Electronics" },   
-  // ]
-  //const listOfProducts = products.filter( product => product.category === 'Electronics' && product.price <= 70);
-  //console.log(listOfProducts);
-  /*
-  //BONUS
-  //[
-  //  {department: 'IT', totalSalary: 320000}
-  //  {department: 'HR', totalSalary: 215000}
-  //]
-  const departmentName2 = employees.filter(employee => (employee.department === "IT"));
-  const ITsalaries = departmentName2.map(employee => employee.salary);
-  const ITsum = Math.floor(ITsalaries.reduce((total, current) => total + current, 0 ));
-
-  const ITResult = [{department: 'IT', totalSalary: ITsum}];
-  // BONUS
-  // [
-  //   { department: 'HR', average: 71666 }
-  // ]
-
-  const departmentName  =  employees.filter(employee => (employee.department === "HR"));
-  const salaries = departmentName.map(employee => employee.salary);
-  const average = Math.floor(salaries.reduce((total, current) => total + current, 0 )/departmentName.length);
-  const HRsum = Math.floor(salaries.reduce((total, current) => total + current, 0 ));
-
-  const AllResult = [{department: 'IT', totalSalary: ITsum},{department: 'HR', totalSalary: HRsum}];
-  const expectedResult = [{department : 'HR', average : average}];
-
-  //console.log(ITResult);
-  console.log(AllResult);
-  console.log(expectedResult);
-  */
+  
 const departments = employees.reduce((dept, employee) => {
   if(!dept[employee.department]){
     dept[employee.department] = {department : employee.department, totalSalary : 0, numOfEmployees : 0, average : 0};
@@ -81,7 +46,6 @@ const departmentAverage = Object.entries(departments).map(([key, value]) => ({
 const highestPayingDepartment = departmentAverage.reduce((highest, current) => {
   return (highest.average > current.average) ? highest : current;
 },{});
-//console.log(departments);
-//console.log(departmentAverage);
+
 console.log(eachDepartment);
 console.log(highestPayingDepartment);
